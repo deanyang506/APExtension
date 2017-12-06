4b5b2010b98a932fdc67ec25e0484f655c51850f
Dean.Yang <yangweichang@aipai.com>
2017-12-06 17:21:22 +0800

    增加虚线

--- APExtension/UIView+APExtension.h
+++ APExtension/UIView+APExtension.h
@@ -57,4 +57,14 @@ typedef NS_OPTIONS(NSUInteger, UIBorderSideType) {
 - (CALayer *)borderForColor:(UIColor *)color borderWidth:(CGFloat)borderWidth borderType:(UIBorderSideType)borderType;
 
 
+/**
+ *  通过 CAShapeLayer 方式绘制虚线
+ *
+ *  param lineLength:     虚线的宽度
+ *  param lineSpacing:    虚线的间距
+ *  param lineColor:      虚线的颜色
+ *  param lineDirection   虚线的方向  YES 为水平方向， NO 为垂直方向
+ **/
+- (void)drawDashLineWithLineLength:(int)lineLength lineSpacing:(int)lineSpacing lineColor:(UIColor *)lineColor lineDirection:(BOOL)isHorizonal;
+
 @end
