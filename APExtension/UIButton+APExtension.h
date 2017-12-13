27a3c5f1dbcf7ef4569f5053725d2df53e639c00
Dean.Yang <yangweichang@aipai.com>
2017-12-13 19:23:00 +0800

    按钮图片文字位置

--- APExtension/UIButton+APExtension.h
+++ APExtension/UIButton+APExtension.h
@@ -8,6 +8,13 @@
 
 #import <UIKit/UIKit.h>
 
+typedef NS_ENUM(NSUInteger, APButtonEdgeInsetsStyle) {
+    APButtonEdgeInsetsStyleTop, // image在上，label在下
+    APButtonEdgeInsetsStyleLeft, // image在左，label在右
+    APButtonEdgeInsetsStyleBottom, // image在下，label在上
+    APButtonEdgeInsetsStyleRight // image在右，label在左
+};
+
 @interface UIButton (APExtension)
 
 /**
@@ -30,5 +37,13 @@
  *  @param right  右间距
  */
 - (void)setEnlargeEdgeWithTop:(CGFloat) top left:(CGFloat) left bottom:(CGFloat) bottom right:(CGFloat) right;
+
+/**
+ *  设置button的titleLabel和imageView的布局样式，及间距
+ *
+ *  @param style titleLabel和imageView的布局样式
+ *  @param space titleLabel和imageView的间距
+ */
+- (void)imageTitleInsert_layoutButtonWithEdgeInsetsStyle:(APButtonEdgeInsetsStyle)style  imageTitleSpace:(CGFloat)space;
 
 @end
--- APExtension/UIButton+APExtension.m
+++ APExtension/UIButton+APExtension.m
@@ -63,4 +63,55 @@ static char rightNameKey;
     return CGRectContainsPoint(rect, point) ? YES : NO;
 }
 
+- (void)imageTitleInsert_layoutButtonWithEdgeInsetsStyle:(APButtonEdgeInsetsStyle)style
+                                         imageTitleSpace:(CGFloat)space {
+    CGFloat imageWith = self.imageView.frame.size.width;
+    CGFloat imageHeight = self.imageView.frame.size.height;
+    CGFloat labelWidth = 0.0;
+    CGFloat labelHeight = 0.0;
+    //    if ([UIDevice currentDevice].systemVersion.floatValue >= 8.0) {
+    //        labelWidth = self.titleLabel.intrinsicContentSize.width;
+    //        labelHeight = self.titleLabel.intrinsicContentSize.height;
+    //    } else {
+    labelWidth = self.titleLabel.frame.size.width;
+    labelHeight = self.titleLabel.frame.size.height;
+    //    }
+    
+    UIEdgeInsets imageEdgeInsets = UIEdgeInsetsZero;
+    UIEdgeInsets labelEdgeInsets = UIEdgeInsetsZero;
+    
+    // 3. 根据style和space得到imageEdgeInsets和labelEdgeInsets的值
+    switch (style) {
+        case APButtonEdgeInsetsStyleTop:
+        {
+            imageEdgeInsets = UIEdgeInsetsMake(-labelHeight-space/2.0, 0, 0, -labelWidth);
+            labelEdgeInsets = UIEdgeInsetsMake(0, -imageWith, -imageHeight-space/2.0, 0);
+        }
+            break;
+        case APButtonEdgeInsetsStyleLeft:
+        {
+            imageEdgeInsets = UIEdgeInsetsMake(0, -space/2.0, 0, space/2.0);
+            labelEdgeInsets = UIEdgeInsetsMake(0, space/2.0, 0, -space/2.0);
+        }
+            break;
+        case APButtonEdgeInsetsStyleBottom:
+        {
+            imageEdgeInsets = UIEdgeInsetsMake(0, 0, -labelHeight-space/2.0, -labelWidth);
+            labelEdgeInsets = UIEdgeInsetsMake(-imageHeight-space/2.0, -imageWith, 0, 0);
+        }
+            break;
+        case APButtonEdgeInsetsStyleRight:
+        {
+            imageEdgeInsets = UIEdgeInsetsMake(0, labelWidth+space/2.0, 0, -labelWidth-space/2.0);
+            labelEdgeInsets = UIEdgeInsetsMake(0, -imageWith-space/2.0, 0, imageWith+space/2.0);
+        }
+            break;
+        default:
+            break;
+    }
+    // 4. 赋值
+    self.titleEdgeInsets = labelEdgeInsets;
+    self.imageEdgeInsets = imageEdgeInsets;
+}
+
 @end
