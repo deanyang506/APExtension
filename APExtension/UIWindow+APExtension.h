4c645a19578704f11c48cdd6f1cb52be91c2d061
Dean.Yang <yangweichang@aipai.com>
2018-01-16 14:28:01 +0800

    修正window topvc

--- APExtension/UIWindow+APExtension.h
+++ APExtension/UIWindow+APExtension.h
@@ -9,17 +9,10 @@
 #import <UIKit/UIKit.h>
 
 @interface UIWindow (APExtension)
-/**
- *  返回该Window下的第一响应者
- *
- *  @return UIResponder对象
- */
-- (UIResponder*)firstResponder NS_AVAILABLE_IOS(2_0);
-/**
- *  返回Window下的顶层视图控制器（能接收旋屏事件的，非navigationController/tabBarController下的子控制器）
- *
- *  @return UIViewController对象
- */
-- (UIViewController*)topViewController NS_AVAILABLE_IOS(5_0);
+
+- (UIResponder *)firstResponder;
+- (UIViewController *)topViewController;
+- (UIViewController *)currentShowController;
+- (UIViewController *)currentShowBottomController;
 
 @end
--- APExtension/UIWindow+APExtension.m
+++ APExtension/UIWindow+APExtension.m
@@ -10,17 +10,62 @@
 
 @implementation UIWindow (APExtension)
 
-- (UIResponder*)firstResponder {
+
+- (UIResponder *)firstResponder {
     return [self valueForKey:@"_firstResponder"];
 }
 
+- (UIViewController *)currentShowController {
+    return [[self class] topViewController:self.rootViewController];
+}
+
++ (UIViewController *)topViewController:(UIViewController *)rootViewController {
+    if ([rootViewController isKindOfClass:[UINavigationController class]]) {
+        UINavigationController *navigationController = (UINavigationController *)rootViewController;
+        return [[self class] topViewController:[navigationController.viewControllers lastObject]];
+    }
+    if ([rootViewController isKindOfClass:[UITabBarController class]]) {
+        UITabBarController *tabController = (UITabBarController *)rootViewController;
+        return [[self class] topViewController:tabController.selectedViewController];
+    }
+    if (rootViewController.presentedViewController) {
+        return [[self class] topViewController:rootViewController.presentedViewController];
+    }
+    return rootViewController;
+}
+
 - (UIViewController*)topViewController {
-    UIViewController *rootViewController = [UIApplication sharedApplication].keyWindow.rootViewController;
-    UIViewController *topViewController = rootViewController;
+    UIViewController *topViewController = self.rootViewController;
     while (topViewController.presentedViewController) {
         topViewController = topViewController.presentedViewController;
     }
     return topViewController;
+}
+
+- (UIViewController *)currentShowBottomController {
+    UIViewController *viewController = self.topViewController;
+    UIViewController *presentingViewController = viewController.presentingViewController;
+    while (presentingViewController) {
+        if (presentingViewController.presentingViewController && [presentingViewController.presentingViewController isViewLoaded] && presentingViewController.presentingViewController.view.window) {
+            presentingViewController = presentingViewController.presentingViewController;
+        } else {
+            break;
+        }
+    }
+    
+    if (presentingViewController) {
+        viewController = presentingViewController;
+    }
+    
+    if ([viewController isKindOfClass:[UITabBarController class]]) {
+        viewController = ((UITabBarController *)viewController).selectedViewController;
+    }
+    
+    if ([viewController isKindOfClass:[UINavigationController class]]) {
+        viewController = ((UINavigationController *)viewController).topViewController;
+    }
+    
+    return viewController;
 }
 
 @end
